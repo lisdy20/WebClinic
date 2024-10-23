cb3cfa2121a66320d624af1c2c9d63a91515c585
Omar Pérez <aomarperez17@gmail.com>
2024-10-22 20:07:31 -0600

    corrección

--- apps/medicamentos/urls.py
+++ apps/medicamentos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lista_citas/', views.ListaMedicamentoTemplateView.as_view(),name='lista-medicamento'),
-    path('agregar_citas/', views.MedicamentoCreateView.as_view(),name='agregar-medicamento'),
+    path('lista_medicamentos/', views.ListaMedicamentoTemplateView.as_view(),name='lista-medicamento'),
+    path('agregar_medicamento/', views.MedicamentoCreateView.as_view(),name='agregar-medicamento'),
 ]
